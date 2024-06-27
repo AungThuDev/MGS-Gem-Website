a329405c32b4078a0b72d6f7bbeb486e52b25879
AungThuDev <htutthu1991@gmail.com>
2024-06-27 16:14:20 +0630

    datatable added,templatesetup

---
+++ database/migrations/2024_06_27_090629_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->date('buy_date');
+            $table->date('sell_date');
+            $table->bigInteger('length');
+            $table->bigInteger('width');
+            $table->bigInteger('height');
+            $table->bigInteger('weight');
+            $table->string('origin');
+            $table->longText('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend.layouts.master');
+});
+Route::get('/products',function (){
+    return view('backend.products.index');
 });
